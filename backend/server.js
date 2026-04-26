9a6fb3dabb4e27ffd7a6501f8c18d950f127d524
yashasl <yashasl2006@gmail.com>
2026-04-26 12:21:34 +0530

    fix start command and cors for production

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: ['http://localhost:3000', process.env.CLIENT_URL].filter(Boolean),
   credentials: true,
 }));
 app.use(express.json());
